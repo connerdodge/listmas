f40ba905a9d5a46848b9ae12393859a5e14dad48
Conner Dodge <cdodge@squareup.com>
2024-12-20 11:39:41 -0800

    Update configs

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from "vitest/config";
 import { sveltekit } from '@sveltejs/kit/vite';
 
 export default defineConfig({
-    base: '/listmas/',
     // @ts-expect-error - this is a valid Vite plugin
     plugins: [sveltekit()],
     test: {
